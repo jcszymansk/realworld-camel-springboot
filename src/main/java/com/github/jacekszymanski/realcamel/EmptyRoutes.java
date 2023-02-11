38c015fb7aab58d7e970c6544e5f85b82da09667
jacek szymanski <jacek.p.szymanski@gmail.com>
2023-02-12 00:00:00 +0100

    minor
    * versions
    * add cli connector
    * empty routes to log

---
+++ src/main/java/com/github/jacekszymanski/realcamel/EmptyRoutes.java
@@ -0,0 +1,55 @@
+package com.github.jacekszymanski.realcamel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.TemplatedRouteBuilder;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class EmptyRoutes extends RouteBuilder {
+
+  private static final List<String> STARTER_URIS = List.of(
+      "direct:createArticle",
+      "direct:deleteArticle",
+      "direct:getArticle",
+      "direct:getArticles",
+      "direct:getArticlesFeed",
+      "direct:updateArticle",
+      "direct:createArticleComment",
+      "direct:deleteArticleComment",
+      "direct:getArticleComments",
+      "direct:createArticleFavorite",
+      "direct:deleteArticleFavorite",
+      "direct:followUserByUsername",
+      "direct:getProfileByUsername",
+      "direct:unfollowUserByUsername",
+      "direct:getTags",
+      "direct:createUser",
+      "direct:getCurrentUser",
+      "direct:login",
+      "direct:updateCurrentUser"
+  );
+
+  @Override
+  public void configure() throws Exception {
+
+    routeTemplate("emptyRouteTemplate").
+        templateParameter("startUri").
+        from("{{startUri}}").
+        log("{{startUri}} was called");
+
+
+    for (String starterUri : STARTER_URIS) {
+      final String id = starterUri.replace(':', '_');
+
+      templatedRoute("emptyRouteTemplate").
+          routeId(id).
+          parameter("startUri", starterUri).
+          prefixId(id);
+    }
+  }
+
+}

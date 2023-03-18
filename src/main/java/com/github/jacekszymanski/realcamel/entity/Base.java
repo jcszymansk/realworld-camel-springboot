3f6639ac811df088197f459240ab1633bf13306d
jszymanski <jacek.p.szymanski@gmail.com>
2023-03-19 00:00:00 +0100

    refactor: use lombok

--- src/main/java/com/github/jacekszymanski/realcamel/entity/Base.java
+++ src/main/java/com/github/jacekszymanski/realcamel/entity/Base.java
@@ -1,12 +1,16 @@
 package com.github.jacekszymanski.realcamel.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
+@Getter
+@Setter
 public class Base {
 
   @Id
@@ -14,11 +18,4 @@ public class Base {
   @JsonIgnore
   private Long id;
 
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
 }
--- src/main/java/com/github/jacekszymanski/realcamel/entity/User.java
+++ src/main/java/com/github/jacekszymanski/realcamel/entity/User.java
@@ -1,5 +1,9 @@
 package com.github.jacekszymanski.realcamel.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.*;
 
 @Entity
@@ -8,6 +12,9 @@ import javax.persistence.*;
     @NamedQuery(name = "selectByEmail", query = "select u from User u where u.email = :email"),
     @NamedQuery(name = "selectByUsername", query = "select u from User u where u.username = :username")
 })
+@Getter
+@Setter
+@ToString(exclude = "image")
 public class User extends Base {
   @Column(nullable = false, unique = true)
   private String username;
@@ -25,53 +32,4 @@ public class User extends Base {
   @Lob
   private byte[] image;
 
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getBio() {
-    return bio;
-  }
-
-  public void setBio(String bio) {
-    this.bio = bio;
-  }
-
-  public byte[] getImage() {
-    return image;
-  }
-
-  public void setImage(byte[] image) {
-    this.image = image;
-  }
-
-  @Override
-  public String toString() {
-    return "User{" +
-        "username='" + username + '\'' +
-        ", password='" + password + '\'' +
-        ", email='" + email + '\'' +
-        ", bio='" + bio + '\'' +
-        '}';
-  }
 }

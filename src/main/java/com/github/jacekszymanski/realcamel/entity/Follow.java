93367d536a7454c0b213fa83a22468a03881cd9f
jszymanski <jacek.p.szymanski@gmail.com>
2023-04-02 00:00:00 +0200

    docs: explain the follow class

--- src/main/java/com/github/jacekszymanski/realcamel/entity/Follow.java
+++ src/main/java/com/github/jacekszymanski/realcamel/entity/Follow.java
@@ -12,6 +12,16 @@ import java.io.Serializable;
 @Getter
 @Setter
 @EqualsAndHashCode
+/*
+ * This class is here only for Hibernate to create the table. I don't want it to ever
+ * fetch any instances, esp. since there's so far no API method to get followers' list.
+ *
+ * I could use Hibernate's @Formula, but I decided I wanted to avoid any Hibernate
+ * specific features and use only pure JPA.
+ *
+ * It also doesn't extend Base as it would only force me to declare another unique index
+ * while as it is, Hibernate does exactly what it should.
+ */
 public class Follow implements Serializable {
 
   @Id

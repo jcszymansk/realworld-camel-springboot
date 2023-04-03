e0f0e3769baf52eeed4165ce2e1bb250dda9f802
jszymanski <jacek.p.szymanski@gmail.com>
2023-04-04 00:00:00 +0200

    test: create article

---
+++ src/test/java/com/github/jacekszymanski/realcamel/CreateArticleTest.java
@@ -0,0 +1,57 @@
+package com.github.jacekszymanski.realcamel;
+
+import com.github.jacekszymanski.realcamel.model.CreateArticle201Response;
+import org.apache.camel.Exchange;
+import org.apache.camel.TypeConverter;
+import org.apache.camel.builder.AdviceWith;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.github.jacekszymanski.realcamel.model.CreateArticleRequest;
+import com.github.jacekszymanski.realcamel.testutil.ArticleUtil;
+import com.github.jacekszymanski.realcamel.testutil.UriUtil;
+import com.github.jacekszymanski.realcamel.testutil.UserUtil;
+
+import java.util.Map;
+
+@CamelSpringBootTest
+@SpringBootTest(classes = CamelApplication.class)
+public class CreateArticleTest extends TestsBase {
+
+  public static final String ENTRY_ENDPOINT = "direct:createArticle";
+  public static final String VERIFY_LOGIN_ENDPOINT = "direct:verifyLogin";
+  public static final String SAVE_ENDPOINT = "jpa:com.github.jacekszymanski.realcamel.entity.Article";
+
+  @Test
+  public void testCreateArticle() throws Exception {
+    final CreateArticleRequest req = ArticleUtil.defaultCreateRequest();
+
+    final String mockVerifyLoginEndpointUri = UriUtil.toMockUri(VERIFY_LOGIN_ENDPOINT);
+
+    final MockEndpoint mockVerifyLoginEndpoint =
+        camelContext.getEndpoint(mockVerifyLoginEndpointUri, MockEndpoint.class);
+
+    AdviceWith.adviceWith(camelContext, UriUtil.fromEndpointToRouteId(ENTRY_ENDPOINT),
+        a -> {
+          a.weaveByToUri(VERIFY_LOGIN_ENDPOINT).replace().to(mockVerifyLoginEndpoint);
+          a.weaveByToUri(SAVE_ENDPOINT).remove();
+        });
+
+    mockVerifyLoginEndpoint.whenAnyExchangeReceived(e -> {
+      e.setProperty("loggedInUser", UserUtil.defaultUserEntity());
+    });
+
+    final Exchange result = producerTemplate.request(ENTRY_ENDPOINT, e -> {
+      e.getIn().setBody(req);
+    });
+
+    Assertions.assertEquals(201, result.getMessage().getHeader(Exchange.HTTP_RESPONSE_CODE));
+
+
+
+  }
+
+}
---
+++ src/test/java/com/github/jacekszymanski/realcamel/testutil/ArticleUtil.java
@@ -0,0 +1,27 @@
+package com.github.jacekszymanski.realcamel.testutil;
+
+import java.util.List;
+
+import com.github.jacekszymanski.realcamel.entity.Article;
+import com.github.jacekszymanski.realcamel.model.CreateArticleRequest;
+import com.github.jacekszymanski.realcamel.model.NewArticle;
+
+public class ArticleUtil {
+
+  public static final String ARTICLE_TITLE = "Article title";
+  public static final String ARTICLE_BODY = "Article body";
+  public static final String ARTICLE_DESCRIPTION = "Article description";
+  public static final List<String> ARTICLE_TAGS = List.of("tag1", "tag2", "tag3");
+
+  public static CreateArticleRequest defaultCreateRequest() {
+    final NewArticle article = new NewArticle()
+      .title(ARTICLE_TITLE)
+      .body(ARTICLE_BODY)
+      .description(ARTICLE_DESCRIPTION)
+      .tagList(ARTICLE_TAGS);
+
+    final CreateArticleRequest req = new CreateArticleRequest().article(article);
+
+    return req;
+  }
+}

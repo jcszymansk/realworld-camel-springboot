8980e05b81bb0b2c8069d94d67611ed34ec2691d
jszymanski <jacek.p.szymanski@gmail.com>
2023-04-16 00:00:00 +0200

    feat: add comment entity

---
+++ src/main/java/com/github/jacekszymanski/realcamel/entity/Comment.java
@@ -0,0 +1,46 @@
+package com.github.jacekszymanski.realcamel.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@Setter
+@ToString(callSuper = true, exclude = { "body", "article" })
+@EqualsAndHashCode(callSuper = true, exclude = { "body", "article" })
+public class Comment extends Base {
+
+  @Column(nullable = false)
+  private String body;
+
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "author", nullable = false)
+  private User author;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "article", nullable = false)
+  @JsonIgnore
+  private Article article;
+
+  @Column(nullable = false)
+  private Date createdAt;
+
+  @PrePersist
+  public void prepare() {
+    createdAt = new Date();
+  }
+
+}

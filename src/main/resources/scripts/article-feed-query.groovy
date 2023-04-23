1e8bb99c73ee804cadb3346b8b33d4f90ec19a1e
jszymanski <jacek.p.szymanski@gmail.com>
2023-04-24 00:00:00 +0200

    feat: sanity check limit/offset

--- src/main/resources/scripts/article-feed-query.groovy
+++ src/main/resources/scripts/article-feed-query.groovy
@@ -9,10 +9,6 @@ if (author != null) params.author = author
 if (liker != null) params.liker = liker
 request.headers['CamelJpaParameters'] = params
 
-// limit and offset
-request.headers['CamelXJpaMaximumResults'] = (request.headers.limit ?: 0) as Integer
-request.headers['CamelXJpaFirstResult'] = (request.headers.offset ?: 0) as Integer
-
 // and the query
 result = """\
     from Article a \
--- src/main/resources/scripts/limit-offset.groovy
+++ src/main/resources/scripts/limit-offset.groovy
@@ -1,3 +1,21 @@
+import com.github.jacekszymanski.realcamel.RouteException
+
+def limit(limit) {
+  switch(limit as Integer) {
+    case null: return 10
+    case { it <= 0 }: throw new RouteException("Limit must be greater than 0")
+    case { it > 100 }: return 100
+    default: return limit as Integer
+  }
+}
+
+def offset(offset) {
+  switch(offset as Integer) {
+    case null: return 0
+    case { it < 0 }: throw new RouteException("Offset must be greater than or equal to 0")
+    default: return offset as Integer
+  }
+}
 // limit and offset
-request.headers['CamelXJpaMaximumResults'] = (request.headers.limit ?: 0) as Integer
-request.headers['CamelXJpaFirstResult'] = (request.headers.offset ?: 0) as Integer
+request.headers['CamelXJpaMaximumResults'] = limit(request.headers.limit)
+request.headers['CamelXJpaFirstResult'] = offset(request.headers.offset)

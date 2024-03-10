87b6f6e669e18a3e411c1b9fa9c598068067ebce
jacek szymanski <jacek.p.szymanski@gmail.com>
2024-03-11 00:00:00 +0100

    refactor: use closures instead of result in groovy

--- src/main/resources/scripts/article-feed-query.groovy
+++ src/main/resources/scripts/article-feed-query.groovy
@@ -1,22 +1,21 @@
-def tag = request.headers.tag
-def author = request.headers.author
-def liker = request.headers.favorited
+{ tag, author, liker ->
 
 // prepare parameters
-def params = [:]
-if (tag != null) params.tag = tag
-if (author != null) params.author = author
-if (liker != null) params.liker = liker
-request.headers['CamelJpaParameters'] = params
+  def params = [:]
+  if (tag != null) params.tag = tag
+  if (author != null) params.author = author
+  if (liker != null) params.liker = liker
+  request.headers['CamelJpaParameters'] = params
 
 // and the query
-result = """\
-    from Article a \
-      ${tag == null ? '' : ', Tag t'} \
-      ${liker == null ? '' : ', Favorite f'} \
-    where \
-          1 = 1 \
-      ${author == null ? '' : 'and a.author.username = :author'} \
-      ${tag == null ? '' : 'and t.article.id = a.id and t.tag = :tag'} \
-      ${liker == null ? '' : 'and f.article.id = a.id and f.user.username = :liker'} \
+  """\
+  from Article a \
+    ${tag == null ? '' : ', Tag t'} \
+    ${liker == null ? '' : ', Favorite f'} \
+  where \
+        1 = 1 \
+    ${author == null ? '' : 'and a.author.username = :author'} \
+    ${tag == null ? '' : 'and t.article.id = a.id and t.tag = :tag'} \
+    ${liker == null ? '' : 'and f.article.id = a.id and f.user.username = :liker'} \
 """
+}(request.headers.tag, request.headers.author, request.headers.favorited)
--- src/main/resources/scripts/resolve-slug.groovy
+++ src/main/resources/scripts/resolve-slug.groovy
@@ -1,11 +1,11 @@
-def slug = request.headers.'slug'
-
-if (slug.matches('\\d+$')) {
-  result = Long.parseLong(slug)
-}
-else if (slug.matches('.*-\\d+$')) {
-  result = slug.replaceFirst('.*-(\\d+)$', '$1')
-}
-else {
-  throw new com.github.jacekszymanski.realcamel.RouteException("Invalid slug: ${slug}")
-}
+{ slug ->
+  if (slug.matches('\\d+$')) {
+    Long.parseLong(slug)
+  }
+  else if (slug.matches('.*-\\d+$')) {
+    slug.replaceFirst('.*-(\\d+)$', '$1')
+  }
+  else {
+    throw new com.github.jacekszymanski.realcamel.RouteException("Invalid slug: ${slug}")
+  }
+}(request.headers.slug)
